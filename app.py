5ccfed9e4ed0800bfe8aa76d6a4ba283a0c50105
Xerozzz <60227509+Xerozzz@users.noreply.github.com>
2023-12-23 17:49:15 +0800

    Added checks for existence of folders

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
-import yfinance as yf, pandas as pd, shutil, os, time, glob, smtplib, ssl
+# Import libraries
+import yfinance as yf
+import pandas as pd
+import shutil
+import os
+import time
+import glob
+import smtplib
+import ssl
 from get_all_tickers import get_tickers as gt
 
-from erase import erase
+# Import python files
 from getData import getData
 from getStats import getStats
 from sendEmail import sendEmail
 
 # Set Up
+# Check if stock folder exists, if not create it
+if not (os.path.exists("Daily_Stock_Report")):
+    os.makedirs("Daily_Stock_Report")
+if not (os.path.exists("Daily_Stock_Report\\Stocks")):
+    os.makedirs("Daily_Stock_Report\\Stocks")
+
 dirname = os.path.dirname(__file__)
 folder = os.path.join(dirname, 'Daily_Stock_Report\\Stocks')
 stocks = ["GOOG", "AMZN", "NVDA"]
-
-# Removes old Stock Folder and recreates it to remove old stocks
-erase(folder)
 
 # Get Stock Data
 getData(stocks, dirname)
@@ -22,4 +33,3 @@ getData(stocks, dirname)
 
 # Send email report to your gmail
 # sendEmail(dirname)
-
