779a2fa1a3cf6882753318382f1b6c9bfc562560
Xerozzz <60227509+Xerozzz@users.noreply.github.com>
2023-12-23 21:26:45 +0800

    .

---
+++ analysis.py
@@ -0,0 +1,11 @@
+tests = {
+    "signal": False,
+    "d/e": False,
+    "pe": False,
+    "beta": False,
+    "fcf": False,
+    "roe": False,
+    "peg": False,
+    "pb": False,
+    "graph": False,
+}
