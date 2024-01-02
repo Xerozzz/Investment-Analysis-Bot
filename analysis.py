21130f858eb69c13e838d1edebbf6dea7e6356ac
Xerozzz <60227509+Xerozzz@users.noreply.github.com>
2024-01-02 18:08:49 +0800

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -9,3 +9,26 @@ tests = {
     "pb": False,
     "graph": False,
 }
+import os
+import pandas as pd
+
+# Set pandas display options
+pd.set_option('display.float_format', '{:.2f}'.format)
+
+def analysis(dirname, folder):
+    test = tests.copy()
+    stocks = os.listdir(folder)
+    indicators = pd.read_csv(os.path.join(dirname, 'Daily_Stock_Report\\Indicators.csv'), index_col='Stocks')
+
+    for i in stocks:
+        df = pd.read_csv(os.path.join(folder, i))
+        values = indicators.loc[os.path.splitext(i)[0]]
+        if vales["beta"] <= 0:
+            test["beta"] = True
+        if values["priceToBook"] > 1:
+            test["pb"] = True
+        if 
+
+dirname = os.path.dirname(__file__)
+folder = os.path.join(dirname, 'Daily_Stock_Report\\Stocks')
+analysis(dirname, folder)
